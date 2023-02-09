8ac313dcdfee77538905895b5be0afdc50d00d39
geoffreyjiang <geoffreychjiang@gmail.com>
2023-02-08 19:34:44 -0800

    deploy

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,6 @@
 from .db import db
 from .user import User
 from .db import environment, SCHEMA
-from .dm import Message
 # from .followers import Follow
 from .likes import Like
 from .replies import Reply
