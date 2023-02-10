075767e2b84b1f7244d087ea1888d687c7ad435c
geoffreyjiang <geoffreychjiang@gmail.com>
2023-02-09 20:54:58 -0800

    splash css

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -16,15 +16,21 @@ const SplashPage = () => {
     return (
         <>
             <div className="splash-container">
-                <div className="splash-img">
-                    <img
-                        src="https://images.unsplash.com/photo-1617653017137-7f8ceffe96af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJsdWUlMjBiaXJkfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                        alt="img"
-                    ></img>
+                <div className="splash-img-container">
+                    {/* <div className="splash-img">
+                        <img
+                            src="https://images.unsplash.com/photo-1617653017137-7f8ceffe96af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJsdWUlMjBiaXJkfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                            alt="img"
+                        ></img>
+                    </div> */}
                 </div>
                 <div className="splash-things">
+                    <i className="fa-brands fa-twitter fa-3x"></i>
+
                     <h1>Welcome to Twidder!</h1>
                     <h2>Whats Goin On?</h2>
+                </div>
+                <div className="splash-buttons">
                     <LoginModal />
                     <SignUpModal />
                     <button
