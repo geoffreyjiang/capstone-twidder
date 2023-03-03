6fbc23afcb9b9aec8182f49b6070255b6e1e39e0
geoffreyjiang <geoffreychjiang@gmail.com>
2023-03-03 11:13:38 -0800

    deploy

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -3,7 +3,7 @@ from .users import seed_users, undo_users
 from .tweets import seed_tweets, undo_tweets
 from .reply import seed_replies, undo_replies
 from .likes import seed_likes, undo_likes
-from .follow import seed_follow, undo_follow
+# from .follow import seed_follow, undo_follow
 from app.models.db import db, environment, SCHEMA
 
 # Creates a seed group to hold our commands
@@ -19,7 +19,7 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_follow()
+        # undo_follow()
         undo_likes()
         undo_replies()
         undo_tweets()
@@ -35,7 +35,7 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_follow()
+    # undo_follow()
     undo_likes()
     undo_replies()
     undo_tweets()
--- app/seeds/follow.py
+++ app/seeds/follow.py
@@ -1,28 +1,28 @@
-from app.models import db, following, environment, SCHEMA
+# from app.models import db, following, environment, SCHEMA
 
-def seed_follow():
-    one = following(
-        main_id=1,
-        followed_id=2
-    )
+# def seed_follow():
+#     one = following(
+#         main_id=1,
+#         followed_id=2
+#     )
 
-    db.session.add(one)
-    db.session.commit()
-
-
+#     db.session.add(one)
+#     db.session.commit()
 
 
-# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
-# have a built in function to do this. With postgres in production TRUNCATE
-# removes all the data from the table, and RESET IDENTITY resets the auto
-# incrementing primary key, CASCADE deletes any dependent entities.  With
-# sqlite3 in development you need to instead use DELETE to remove all data and
-# it will reset the primary keys for you as well.
 
-def undo_follow():
-    if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.following RESTART IDENTITY CASCADE;")
-    else:
-        db.session.execute("DELETE FROM following")
 
-    db.session.commit()
+# # Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# # have a built in function to do this. With postgres in production TRUNCATE
+# # removes all the data from the table, and RESET IDENTITY resets the auto
+# # incrementing primary key, CASCADE deletes any dependent entities.  With
+# # sqlite3 in development you need to instead use DELETE to remove all data and
+# # it will reset the primary keys for you as well.
+
+# def undo_follow():
+#     if environment == "production":
+#         db.session.execute(f"TRUNCATE table {SCHEMA}.following RESTART IDENTITY CASCADE;")
+#     else:
+#         db.session.execute("DELETE FROM following")
+
+#     db.session.commit()
