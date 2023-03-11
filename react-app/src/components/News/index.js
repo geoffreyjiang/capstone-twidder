8a13c88732bda1b630c4b2dc1830b7aa371f3549
geoffreyjiang <geoffreychjiang@gmail.com>
2023-03-11 00:08:20 -0800

    bug

--- react-app/src/components/News/index.js
+++ react-app/src/components/News/index.js
@@ -1,45 +1,45 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getNews } from "../../store/news";
-import "./index.css";
-import fetchNewsData from "./fetchNews";
-function LatestNews() {
-    // const news = useSelector((state) => Object.values(state.news.sports));
-    // const sports = useSelector((state) => state.news);
-    // console.log(sports);
-    // let sportsNews;
-    // sportsNews = sports.map((el) => el);
-    // console.log(sportsNews);
-    // const dispatch = useDispatch();
-    // useEffect(() => {
-    //     dispatch(getNews());
-    // });
-    // console.log(news);
-    // return (
-    //     <div className="news-container">
-    //         <h1>Latest News</h1>
-    //         {/* <h3>{news}</h3> */}
-    //     </div>
-    // );
+// import React, { useState, useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { getNews } from "../../store/news";
+// import "./index.css";
+// import fetchNewsData from "./fetchNews";
+// function LatestNews() {
+// const news = useSelector((state) => Object.values(state.news.sports));
+// const sports = useSelector((state) => state.news);
+// console.log(sports);
+// let sportsNews;
+// sportsNews = sports.map((el) => el);
+// console.log(sportsNews);
+// const dispatch = useDispatch();
+// useEffect(() => {
+//     dispatch(getNews());
+// });
+// console.log(news);
+// return (
+//     <div className="news-container">
+//         <h1>Latest News</h1>
+//         {/* <h3>{news}</h3> */}
+//     </div>
+// );
 
-    const [newsData, setNewsData] = useState([]);
+//     const [newsData, setNewsData] = useState([]);
 
-    useEffect(() => {
-        fetchNewsData().then((data) => {
-            setNewsData(data);
-        });
-    }, []);
-    console.log(newsData);
-    return (
-        <div>
-            {newsData.map((newsItem) => (
-                <div key={newsItem.id}>
-                    <h3>{newsItem.title}</h3>
-                    <p>{newsItem.url}</p>
-                </div>
-            ))}
-        </div>
-    );
-}
+//     useEffect(() => {
+//         fetchNewsData().then((data) => {
+//             setNewsData(data);
+//         });
+//     }, []);
+//     console.log(newsData);
+//     return (
+//         <div>
+//             {newsData.map((newsItem) => (
+//                 <div key={newsItem.id}>
+//                     <h3>{newsItem.title}</h3>
+//                     <p>{newsItem.url}</p>
+//                 </div>
+//             ))}
+//         </div>
+//     );
+// }
 
-export default LatestNews;
+// export default LatestNews;
